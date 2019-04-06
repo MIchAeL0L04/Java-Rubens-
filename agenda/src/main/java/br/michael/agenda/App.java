bb5148bc017fed4142a15cf0e3c9ebf69376f496
Michael Richard <Michael04descomplica@gmail.com>
2019-04-06 01:03:41 -0300

    Aula Rubens dia 05/04/19 Agenda

---
+++ agenda/src/main/java/br/michael/agenda/App.java
@@ -0,0 +1,13 @@
+package br.michael.agenda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
