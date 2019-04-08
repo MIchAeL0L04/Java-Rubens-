1af8cbc0b64e6f9313ebfe808e405fcfca38e484
Michael Richard <Michael04descomplica@gmail.com>
2019-04-08 13:21:53 -0300

    sioláMundo

---
+++ siOlaMundo/src/siOlaMundo/OlaMundo.java
@@ -0,0 +1,12 @@
+package siOlaMundo;
+
+public class OlaMundo {
+//Michael é muito gato
+	public static void main(String[] args) {
+		String nome ="Michael Leonardo";
+		System.out.println(nome);
+		nome = "Leonardo Michael";
+		System.out.println(nome3);njrenjverjbjrb
+	}
+
+}
